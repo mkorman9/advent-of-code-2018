2a533c473712a2d80e2028dc267a58160a69f43b
mkorman9 <michal.korman@icloud.com>
2018-12-17 15:47:11 +0100

    debugging water flow

--- src/main/kotlin/com/gitlab/mkorman/aoc2018/task17/Main.kt
+++ src/main/kotlin/com/gitlab/mkorman/aoc2018/task17/Main.kt
@@ -17,7 +17,7 @@ class ClayPosition(val x: Int, val y: Int) {
             val match = VerticalStretchPattern.find(record) ?: return null;
             val result = mutableListOf<ClayPosition>()
 
-            for (i in Integer.valueOf(match.groupValues[2]) until Integer.valueOf(match.groupValues[3])) {
+            for (i in Integer.valueOf(match.groupValues[2])..Integer.valueOf(match.groupValues[3])) {
                 result.add(ClayPosition(i, Integer.valueOf(match.groupValues[1])))
             }
 
@@ -28,7 +28,7 @@ class ClayPosition(val x: Int, val y: Int) {
             val match = HorizontalStretchPattern.find(record) ?: return null;
             val result = mutableListOf<ClayPosition>()
 
-            for (i in Integer.valueOf(match.groupValues[2]) until Integer.valueOf(match.groupValues[3])) {
+            for (i in Integer.valueOf(match.groupValues[2])..Integer.valueOf(match.groupValues[3])) {
                 result.add(ClayPosition(Integer.valueOf(match.groupValues[1]), i))
             }
 
@@ -45,19 +45,48 @@ class Map(clayPositions: List<ClayPosition>) {
         val minX = clayPositions.minBy { it.x }!!.x
         val maxX = clayPositions.maxBy { it.x }!!.x + 1
         val maxY = clayPositions.maxBy { it.y }!!.y + 1
-        val verticalSpan = Math.abs(maxX) + Math.abs(minX) + 2
+        val verticalSpan = Math.abs(maxX) - Math.abs(minX) + 2
 
         area = Array(maxY) { y ->
             Array(verticalSpan) { x -> Tile.Sand }
         }
 
-        leftPadding = minX
+        leftPadding = Math.abs(minX)
 
         for (clay in clayPositions) {
             area[indexY(clay.y)][indexX(clay.x)] = Tile.Clay
         }
 
         area[indexY(0)][indexX(500)] = Tile.Water
+    }
+
+    fun print() {
+        for (y in 0 until area.size) {
+            for (x in 0 until area[y].size) {
+                if (area[y][x] == Tile.Sand) {
+                    print(".")
+                } else if (area[y][x] == Tile.Clay) {
+                    print("#")
+                } else if (area[y][x] == Tile.Water) {
+                    print("~")
+                }
+            }
+
+            println()
+        }
+
+        println()
+    }
+
+    fun flowUntilComplete() {
+        while (true) {
+            val updates = update()
+            if (updates == 0) {
+                break
+            }
+
+            print()
+        }
     }
 
     fun countWaterTiles(): Int {
@@ -74,7 +103,52 @@ class Map(clayPositions: List<ClayPosition>) {
         return result
     }
 
-    private fun indexX(index: Int) = index + leftPadding
+    private fun update(): Int {
+        var updates = 0
+
+        for (y in 0 until area.size) {
+            for (x in 0 until area[y].size) {
+                if (area[y][x] == Tile.Water) {
+                    if (waterFlow(x, y)) {
+                        updates++
+                    }
+                }
+            }
+        }
+
+        return updates
+    }
+
+    private fun waterFlow(x: Int, y: Int): Boolean {
+        var flow = false
+
+        if (y + 1 < area.size && area[y + 1][x] == Tile.Sand) {
+            area[y + 1][x] = Tile.Water
+            flow = true
+        } else {
+            if (y + 1 < area.size) {
+                if (x - 1 >= 0
+                    && area[y][x - 1] != Tile.Clay
+                    && area[y + 1][x - 1] != Tile.Sand
+                ) {
+                    area[y][x - 1] = Tile.Water
+                    flow = true
+                }
+
+                if (x + 1 < area[0].size
+                    && area[y][x + 1] == Tile.Sand
+                    && area[y + 1][x + 1] != Tile.Sand
+                ) {
+                    area[y][x + 1] = Tile.Water
+                    flow = true
+                }
+            }
+        }
+
+        return flow
+    }
+
+    private fun indexX(index: Int) = index - leftPadding + 1
     private fun indexY(index: Int) = index
 
     enum class Tile {
@@ -85,10 +159,12 @@ class Map(clayPositions: List<ClayPosition>) {
 }
 
 fun main(args: Array<String>) {
-    val source = File(object {}.javaClass.getResource("/task17/input.txt").file);
+    val source = File(object {}.javaClass.getResource("/task17/debug.txt").file);
     val clayPositions: List<ClayPosition> = source.readLines()
         .flatMap { ClayPosition.parseRecord(it) }
     val map = Map(clayPositions)
 
-    println(map.countWaterTiles())
+    map.flowUntilComplete()
+
+    println("Subtask #1: ${map.countWaterTiles()}")
 }
